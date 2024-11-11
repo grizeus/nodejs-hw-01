7cdb686dbe3fad74dc3dab3044bd232811a588b1
grizeus <zaytsewww@gmail.com>
2024-11-11 20:00:23 +0200

    .

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,14 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts";
+
+export const countContacts = async () => {
+    const jsonData = await readContacts();
+    let dataArray = JSON.parse(jsonData);
+
+    if (!Array.isArray(dataArray)) {
+        return 0;
+    }
+
+    return dataArray.length;
+};
 
 console.log(await countContacts());
