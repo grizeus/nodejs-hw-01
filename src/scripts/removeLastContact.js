5883b2d4d87214a95c14b88b30b86a92dd2dd51b
grizeus <zaytsewww@gmail.com>
2024-11-11 20:08:59 +0200

    .

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,26 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from "../utils/writeContacts";
+import { readContacts } from "../utils/readContacts";
+
+export const removeLastContact = async () => {
+  try {
+    const jsonData = await readContacts();
+    let dataArray = JSON.parse(jsonData);
+
+    if (!Array.isArray(dataArray)) {
+      return;
+    }
+
+    if (dataArray.length === 0) {
+      return;
+    }
+
+    dataArray.pop();
+    const updatedJson = JSON.stringify(dataArray, null, 2);
+
+    await writeContacts(updatedJson);
+  } catch (e) {
+    console.error(e.message);
+  }
+};
 
 removeLastContact();
